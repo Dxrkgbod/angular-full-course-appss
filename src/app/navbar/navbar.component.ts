93dc05ce1ac2bb883acb5dcb2769a4b5480f0ba7
Dxrkgbod <akachukwuagbodike@gmail.com>
2024-08-27 07:27:15 +0100

    Create navbar and header section

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-navbar',
+    standalone: true,
+    template: ` <h1>Navbar Component</h1> 
+                <p>Dummy Paragraph</p>
+                <button>Submit</button>
+                `,
+    styles: `h1{
+            color: red;
+            background: black
+            }
+            `,
+       styleUrl: './navbar.component.css'
+       
+
+})
+
+export class AppNavbar { }
